d23df726c14a5a37b17fb4d68f4e3503f3951ce3
dima <greshchook@gmail.com>
2017-12-05 09:16:22 +0200

    my solution for substring

---
+++ mystr.cpp
@@ -0,0 +1,38 @@
+#include "mystr.hpp"
+
+int strlength(const char* str) {
+    int length = 0;
+    while (*str++) length++;
+    return length;
+}
+
+int strstr(const char *text, const char *pattern)
+{
+    /* ... */
+    int nessesary = strlength(pattern);
+   if (nessesary == 0) {
+        return 0;
+    }
+    const char* tmp = pattern;
+    int id = 0;
+    int actual = 0;
+
+    while (*text) {
+        if (*text == *pattern) {
+            while (*text == *tmp && *text) {
+                tmp++;
+                text++;
+                actual++;
+                id++;
+            }
+            if (actual == nessesary) return id - nessesary;
+        }else{
+            actual = 0;
+            tmp = pattern;
+        }
+
+        text++;
+        id++;
+    }
+    return  -1;
+}
---
+++ mystr.hpp
@@ -0,0 +1,8 @@
+#ifndef _MYSTR_
+
+#define _MYSTR_
+
+int strlength(const char* str);
+int strstr(const char *text, const char *pattern);
+
+#endif
